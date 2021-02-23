eb3ef6aa5c0c43a1110e14640070add2104897d4
romanolivi <romano_livi@outlook.com>
2021-02-23 00:33:36 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,64 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
+  
+
+#   def categories_attributes=(category_attributes)
+#     category_attributes.values.each do |category_attribute|
+#       category = Category.find_or_create_by(category_attribute)
+#       self.categories << category 
+#     end
+#   end
+  
+# end 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  def categories_attributes=(category_attributes)
+    category_attributes.each do |i, category_attributes|
+      if category_attributes[:name].present?
+      category = Category.find_or_create_by(name: category_attributes[:name])
+        if !self.categories.include?(category)
+          self.post_categories.build(:category => category)
+        end
+      end
 
 
+    end
+  end
 end
+
+
+
+  # def categories_attributes=(category_attributes)
+  #   category_attributes.values.each do |category_attribute|
+  #     if !category_attribute[:name].empty?
+  #       category = Category.find_or_create_by(name: category_attributes[:name])
+  #     category = Category.find_or_create_by(category_attribute)
+  #     end
+
+  #     if !self.post_categories.include?(category)
+  #       self.post_categories.build(:category => category)
+  #     end
+  #   end
+  # end
